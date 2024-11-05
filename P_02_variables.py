c301fa425e2205e0f8f84560496e748fea96e6b6
Miguel <Seacorm24921@masseyhigh.school.nz>
2024-11-06 12:07:10 +1300

    Basic Variables Concept
    Made by Miguel

---
+++ P_02_variables.py
@@ -0,0 +1,27 @@
+# Code to learn about variables...
+
+# Variables can be a string (note the speech marks)
+greeting = "hello world"
+
+# Variables can be a number
+favorite_number = 4
+num_string = "5"
+
+# Variables can be Boolean (True / False)
+love_fantasy = True
+
+# This Variable has already been fixed
+my_name = "Miguel"
+
+# Constants are uppercase
+PI = 3.14
+
+print(greeting)
+print(favorite_number)
+print(love_fantasy)
+print(my_name)
+
+print(favorite_number * 2)
+print(num_string * 4)
+
+a_01_my_num = 4
